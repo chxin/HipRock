d85a985a58bf6d90860366853a6aa06dd759676f
BuildServer <buildserver@BuildServers-Mac-mini.local>
2013-07-31 09:07:22 +0800

    modify dailybuild.sh

--- Build/dailybuild.sh
+++ Build/dailybuild.sh
@@ -1,5 +1,8 @@
 makefilepath=~/BuildFolder/SourceCode/Master/BluesGit/Build/
-
+srcpath=~/BuildFolder/SourceCode/Master/BluesGit
+cd ${srcpath}
+git pull
+cd ${makefilepath}
 #make xctest
 make --makefile=Makefile --directory=${makefilepath} xcbuild
 
