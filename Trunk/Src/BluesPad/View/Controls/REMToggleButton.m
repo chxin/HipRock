978cb9e1cd9084e1027884a0410bcb0ea94c8505
Zilong-Oscar.Xu <zilong@Oscars-MacBook-Pro.local>
2013-08-19 16:14:43 +0800

    Remove useless files. Add toggle btw class

---
+++ Trunk/Src/BluesPad/View/Controls/REMToggleButton.m
@@ -0,0 +1,31 @@
+//
+//  REMToggleButton.m
+//  Blues
+//
+//  Created by Zilong-Oscar.Xu on 8/19/13.
+//
+//
+
+#import "REMToggleButton.h"
+
+@implementation REMToggleButton
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
