95e0992f9aba4fb3ff75f0ca6a8831f3447e1c28
Zilong-Oscar.Xu <zilong@Oscars-MacBook-Pro.local>
2013-09-02 15:30:17 +0800

    alert when unbind weibo

--- Trunk/Src/BluesPad/Controller/Building/REMBuildingSettingViewController.m
+++ Trunk/Src/BluesPad/Controller/Building/REMBuildingSettingViewController.m
@@ -76,10 +76,10 @@
     // weibo account binding cell
     if (indexPath.section == 1 && indexPath.item == 0) {
         [[cell textLabel]setText:@"绑定新浪微博"];
-        UISwitch* s = [[UISwitch alloc]initWithFrame:CGRectMake(405, 12, 79, 27)];
-        s.on = [Weibo.weibo isAuthenticated];
-        [s addTarget:self action:@selector(weiboSwitcherChanged:) forControlEvents:UIControlEventValueChanged];
-        [cell addSubview:s];
+        self.weiboAccoutSwitcher = [[UISwitch alloc]initWithFrame:CGRectMake(405, 12, 79, 27)];
+        self.weiboAccoutSwitcher.on = [Weibo.weibo isAuthenticated];
+        [self.weiboAccoutSwitcher addTarget:self action:@selector(weiboSwitcherChanged:) forControlEvents:UIControlEventValueChanged];
+        [cell addSubview:self.weiboAccoutSwitcher];
     }
     //logout cell
     else if(indexPath.section==0){
@@ -127,23 +127,29 @@
 
 - (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex
 {
-    
-    if(buttonIndex==0){
-        REMUserModel *currentUser = [REMApplicationContext instance].currentUser;
-        REMCustomerModel *currentCustomer = [REMApplicationContext instance].currentCustomer;
-        
-        [currentUser kill];
-        [currentCustomer kill];
-        currentUser = nil;
-        currentCustomer = nil;
-        UINavigationController *nav=(UINavigationController *)self.parentViewController;
-        [nav dismissViewControllerAnimated:YES completion:^(void){
-            [self.navigationController popToRootViewControllerAnimated:YES];
-            [self.splashScreenController showLoginView];
-        }];
-        
-        
-        
+    if ([alertView.accessibilityIdentifier isEqual: @"weiboAccount"]) {
+        if (buttonIndex == 0) {
+            self.weiboAccoutSwitcher.on = YES;
+        } else if (buttonIndex == 1) {
+            [[WeiboAccounts shared]signOut];
+        }
+    } else {
+        if(buttonIndex==0){
+            REMUserModel *currentUser = [REMApplicationContext instance].currentUser;
+            REMCustomerModel *currentCustomer = [REMApplicationContext instance].currentCustomer;
+            
+            [currentUser kill];
+            [currentCustomer kill];
+            currentUser = nil;
+            currentCustomer = nil;
+            UINavigationController *nav=(UINavigationController *)self.parentViewController;
+            [nav dismissViewControllerAnimated:YES completion:^(void){
+                [self.navigationController popToRootViewControllerAnimated:YES];
+                [self.splashScreenController showLoginView];
+            }];
+            
+            
+        }
     }
 }
 
@@ -161,7 +167,12 @@
 - (void) weiboSwitcherChanged:(UISwitch*)sender {
     BOOL isAuthed = [Weibo.weibo isAuthenticated];
     if (sender.on == NO && isAuthed) {
-        [[WeiboAccounts shared]signOut];
+        UIAlertView* alertView = [[UIAlertView alloc] initWithTitle:@"" message:@"解除绑定新浪微博？" delegate:self cancelButtonTitle:nil otherButtonTitles:@"放弃", @"解除绑定", nil];
+        alertView.accessibilityIdentifier = @"weiboAccount";
+       
+        [alertView show];
+//    initWithTitle:@"" message:@"解除绑定新浪微博？" delegate:self cancelButtonTitle:@"放弃" otherButtonTitles:@"解除绑定"
+//        [[WeiboAccounts shared]signOut];
     } else if (!isAuthed && sender.on == YES){
         [Weibo.weibo authorizeWithCompleted:^(WeiboAccount *account, NSError *error) {
             NSString *message = nil;
