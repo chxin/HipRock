901debef78c573c09d9cc7569d604d896d9e3665
Zilong-Oscar.Xu <zilong@Oscars-MacBook-Pro.local>
2013-09-03 10:54:03 +0800

    weibo updated

--- Trunk/Src/BluesPad/Controller/Building/REMBuildingSettingViewController.m
+++ Trunk/Src/BluesPad/Controller/Building/REMBuildingSettingViewController.m
@@ -76,10 +76,10 @@
     // weibo account binding cell
     if (indexPath.section == 1 && indexPath.item == 0) {
         [[cell textLabel]setText:@"绑定新浪微博"];
-        UISwitch* s = [[UISwitch alloc]initWithFrame:CGRectMake(405, 12, 79, 27)];
-        s.on = [Weibo.weibo isAuthenticated];
-        [s addTarget:self action:@selector(weiboSwitcherChanged:) forControlEvents:UIControlEventValueChanged];
-        [cell addSubview:s];
+        self.weiboAccoutSwitcher = [[UISwitch alloc]initWithFrame:CGRectMake(405, 12, 79, 27)];
+        self.weiboAccoutSwitcher.on = [Weibo.weibo isAuthenticated];
+        [self.weiboAccoutSwitcher addTarget:self action:@selector(weiboSwitcherChanged:) forControlEvents:UIControlEventValueChanged];
+        [cell addSubview:self.weiboAccoutSwitcher];
     }
     //logout cell
     else if(indexPath.section==0){
@@ -99,12 +99,9 @@
     else if(indexPath.section==2 && indexPath.row==0 ){
         UITableViewCell *cell1 = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"Cell1"];
         //[[cell textLabel]setText:@"退出登录"];
-        
-        
         cell1.textLabel.text=@"退出登录";
         cell1.textLabel.textColor=[UIColor redColor];
         cell1.textLabel.textAlignment=NSTextAlignmentCenter;
-        
         return cell1;
         //NSLog(@"frame:%@",NSStringFromCGRect(cell.contentView.frame));
         //NSLog(@"bounds:%@",NSStringFromCGRect(cell.contentView.frame));
@@ -130,23 +127,29 @@
 
 - (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex
 {
-    
-    if(buttonIndex==0){
-        REMUserModel *currentUser = [REMApplicationContext instance].currentUser;
-        REMCustomerModel *currentCustomer = [REMApplicationContext instance].currentCustomer;
-        
-        [currentUser kill];
-        [currentCustomer kill];
-        currentUser = nil;
-        currentCustomer = nil;
-        UINavigationController *nav=(UINavigationController *)self.parentViewController;
-        [nav dismissViewControllerAnimated:YES completion:^(void){
-            [self.navigationController popToRootViewControllerAnimated:YES];
-            //[self.splashScreenController showLoginView];
-        }];
-        
-        
-        
+    if ([alertView.accessibilityIdentifier isEqual: @"weiboAccount"]) {
+        if (buttonIndex == 0) {
+            self.weiboAccoutSwitcher.on = YES;
+        } else if (buttonIndex == 1) {
+            [[WeiboAccounts shared]signOut];
+        }
+    } else {
+        if(buttonIndex==0){
+            REMUserModel *currentUser = [REMApplicationContext instance].currentUser;
+            REMCustomerModel *currentCustomer = [REMApplicationContext instance].currentCustomer;
+            
+            [currentUser kill];
+            [currentCustomer kill];
+            currentUser = nil;
+            currentCustomer = nil;
+            UINavigationController *nav=(UINavigationController *)self.parentViewController;
+            [nav dismissViewControllerAnimated:YES completion:^(void){
+                [self.navigationController popToRootViewControllerAnimated:YES];
+                //[self.splashScreenController showLoginView];
+            }];
+            
+            
+        }
     }
 }
 
@@ -164,7 +167,10 @@
 - (void) weiboSwitcherChanged:(UISwitch*)sender {
     BOOL isAuthed = [Weibo.weibo isAuthenticated];
     if (sender.on == NO && isAuthed) {
-        [[WeiboAccounts shared]signOut];
+        UIAlertView* alertView = [[UIAlertView alloc] initWithTitle:@"" message:@"解除绑定新浪微博？" delegate:self cancelButtonTitle:nil otherButtonTitles:@"放弃", @"解除绑定", nil];
+        alertView.accessibilityIdentifier = @"weiboAccount";
+        
+        [alertView show];
     } else if (!isAuthed && sender.on == YES){
         [Weibo.weibo authorizeWithCompleted:^(WeiboAccount *account, NSError *error) {
             NSString *message = nil;
