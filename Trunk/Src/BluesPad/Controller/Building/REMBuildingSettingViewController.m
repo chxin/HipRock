22ba896dc85c0e19bdcf5884c11d13c3d240a421
Zilong-Oscar.Xu <zilong@Oscars-MacBook-Pro.local>
2013-09-03 10:36:10 +0800

    J

--- Trunk/Src/BluesPad/Controller/Building/REMBuildingSettingViewController.m
+++ Trunk/Src/BluesPad/Controller/Building/REMBuildingSettingViewController.m
@@ -39,7 +39,7 @@
     //myView registerClass forCellReuseIdentifier:<#(NSString *)#>
     // Uncomment the following line to preserve selection between presentations.
     // self.clearsSelectionOnViewWillAppear = NO;
- 
+    
     // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
     // self.navigationItem.rightBarButtonItem = self.editButtonItem;
 }
@@ -76,15 +76,15 @@
     // weibo account binding cell
     if (indexPath.section == 1 && indexPath.item == 0) {
         [[cell textLabel]setText:@"绑定新浪微博"];
-        self.weiboAccoutSwitcher = [[UISwitch alloc]initWithFrame:CGRectMake(405, 12, 79, 27)];
-        self.weiboAccoutSwitcher.on = [Weibo.weibo isAuthenticated];
-        [self.weiboAccoutSwitcher addTarget:self action:@selector(weiboSwitcherChanged:) forControlEvents:UIControlEventValueChanged];
-        [cell addSubview:self.weiboAccoutSwitcher];
+        UISwitch* s = [[UISwitch alloc]initWithFrame:CGRectMake(405, 12, 79, 27)];
+        s.on = [Weibo.weibo isAuthenticated];
+        [s addTarget:self action:@selector(weiboSwitcherChanged:) forControlEvents:UIControlEventValueChanged];
+        [cell addSubview:s];
     }
     //logout cell
     else if(indexPath.section==0){
         
-       
+        
         if(indexPath.row==0){
             [[cell textLabel]setText:@"显示名称"];
             NSString *name=[REMApplicationContext instance].currentUser.realname;
@@ -99,26 +99,29 @@
     else if(indexPath.section==2 && indexPath.row==0 ){
         UITableViewCell *cell1 = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"Cell1"];
         //[[cell textLabel]setText:@"退出登录"];
+        
+        
         cell1.textLabel.text=@"退出登录";
         cell1.textLabel.textColor=[UIColor redColor];
         cell1.textLabel.textAlignment=NSTextAlignmentCenter;
+        
         return cell1;
         //NSLog(@"frame:%@",NSStringFromCGRect(cell.contentView.frame));
         //NSLog(@"bounds:%@",NSStringFromCGRect(cell.contentView.frame));
         
         
         /*
-        UIButton *logout= [UIButton buttonWithType:UIButtonTypeRoundedRect];
-        [logout setFrame: CGRectMake(0, 0, 480, cell.contentView.frame.size.height)];
-        
-        logout.layer.borderWidth=0;
-        
-        
-        [logout setTitleColor:[UIColor redColor] forState:UIControlStateNormal];
-        [logout setTitle:@"退出登录" forState:UIControlStateNormal];
-        logout.contentMode=UIViewContentModeScaleAspectFit;
-        [logout addTarget:self action:@selector(logout:) forControlEvents:UIControlEventTouchUpInside];
-        [cell.contentView addSubview:logout];*/
+         UIButton *logout= [UIButton buttonWithType:UIButtonTypeRoundedRect];
+         [logout setFrame: CGRectMake(0, 0, 480, cell.contentView.frame.size.height)];
+         
+         logout.layer.borderWidth=0;
+         
+         
+         [logout setTitleColor:[UIColor redColor] forState:UIControlStateNormal];
+         [logout setTitle:@"退出登录" forState:UIControlStateNormal];
+         logout.contentMode=UIViewContentModeScaleAspectFit;
+         [logout addTarget:self action:@selector(logout:) forControlEvents:UIControlEventTouchUpInside];
+         [cell.contentView addSubview:logout];*/
     }
     return cell;
 }
@@ -127,29 +130,23 @@
 
 - (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex
 {
-    if ([alertView.accessibilityIdentifier isEqual: @"weiboAccount"]) {
-        if (buttonIndex == 0) {
-            self.weiboAccoutSwitcher.on = YES;
-        } else if (buttonIndex == 1) {
-            [[WeiboAccounts shared]signOut];
-        }
-    } else {
-        if(buttonIndex==0){
-            REMUserModel *currentUser = [REMApplicationContext instance].currentUser;
-            REMCustomerModel *currentCustomer = [REMApplicationContext instance].currentCustomer;
-            
-            [currentUser kill];
-            [currentCustomer kill];
-            currentUser = nil;
-            currentCustomer = nil;
-            UINavigationController *nav=(UINavigationController *)self.parentViewController;
-            [nav dismissViewControllerAnimated:YES completion:^(void){
-                [self.navigationController popToRootViewControllerAnimated:YES];
-                [self.splashScreenController showLoginView];
-            }];
-            
-            
-        }
+    
+    if(buttonIndex==0){
+        REMUserModel *currentUser = [REMApplicationContext instance].currentUser;
+        REMCustomerModel *currentCustomer = [REMApplicationContext instance].currentCustomer;
+        
+        [currentUser kill];
+        [currentCustomer kill];
+        currentUser = nil;
+        currentCustomer = nil;
+        UINavigationController *nav=(UINavigationController *)self.parentViewController;
+        [nav dismissViewControllerAnimated:YES completion:^(void){
+            [self.navigationController popToRootViewControllerAnimated:YES];
+            //[self.splashScreenController showLoginView];
+        }];
+        
+        
+        
     }
 }
 
@@ -167,12 +164,7 @@
 - (void) weiboSwitcherChanged:(UISwitch*)sender {
     BOOL isAuthed = [Weibo.weibo isAuthenticated];
     if (sender.on == NO && isAuthed) {
-        UIAlertView* alertView = [[UIAlertView alloc] initWithTitle:@"" message:@"解除绑定新浪微博？" delegate:self cancelButtonTitle:nil otherButtonTitles:@"放弃", @"解除绑定", nil];
-        alertView.accessibilityIdentifier = @"weiboAccount";
-       
-        [alertView show];
-//    initWithTitle:@"" message:@"解除绑定新浪微博？" delegate:self cancelButtonTitle:@"放弃" otherButtonTitles:@"解除绑定"
-//        [[WeiboAccounts shared]signOut];
+        [[WeiboAccounts shared]signOut];
     } else if (!isAuthed && sender.on == YES){
         [Weibo.weibo authorizeWithCompleted:^(WeiboAccount *account, NSError *error) {
             NSString *message = nil;
@@ -188,43 +180,43 @@
     }
 }
 /*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
+ // Override to support conditional editing of the table view.
+ - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
+ {
+ // Return NO if you do not want the specified item to be editable.
+ return YES;
+ }
+ */
 
 /*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    }   
-    else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
+ // Override to support editing the table view.
+ - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+ {
+ if (editingStyle == UITableViewCellEditingStyleDelete) {
+ // Delete the row from the data source
+ [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+ }
+ else if (editingStyle == UITableViewCellEditingStyleInsert) {
+ // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+ }
+ }
+ */
 
 /*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
-{
-}
-*/
+ // Override to support rearranging the table view.
+ - (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
+ {
+ }
+ */
 
 /*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
+ // Override to support conditional rearranging of the table view.
+ - (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
+ {
+ // Return NO if you do not want the item to be re-orderable.
+ return YES;
+ }
+ */
 
 #pragma mark - Table view delegate
 
