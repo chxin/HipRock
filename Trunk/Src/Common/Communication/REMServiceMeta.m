ad4e1506358f025c74aaec04719ed5737b8daa5f
tantan <tan-tan.tan@schneider-electric.com>
2013-09-09 14:58:35 +0800

    change build

--- Trunk/Src/Common/Communication/REMServiceMeta.m
+++ Trunk/Src/Common/Communication/REMServiceMeta.m
@@ -12,6 +12,10 @@
 
 //const static NSString *SVC_BASE_HOST = @"223.4.20.20";
 //const static NSString *SVC_BASE_SCHEMA = @"http";
+//const static NSString *SVC_BASE_SUFFIX = @"/v1.3.0.8/MobileApiHost/";
+//const static NSString *SVC_BASE_HOST = @"10.177.206.79";
+//const static NSString *SVC_BASE_SCHEMA = @"http";
+//const static NSString *SVC_BASE_SUFFIX = @"/Mobile/";
 //const static NSString *SVC_BASE_SUFFIX = @"/v1.3/MobileApiHost/";
 #ifdef DEBUG
 const static NSString *SVC_BASE_HOST = @"10.177.206.79";
