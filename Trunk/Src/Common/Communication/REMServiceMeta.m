0b000d96647663bb383ca172c78405296c5ace09
张 锋 <zhangfeng0408@gmail.com>
2013-09-11 09:12:38 +0800

    fix a bug - Aries

--- Trunk/Src/Common/Communication/REMServiceMeta.m
+++ Trunk/Src/Common/Communication/REMServiceMeta.m
@@ -18,9 +18,9 @@
 //const static NSString *SVC_BASE_SUFFIX = @"/Mobile/";
 //const static NSString *SVC_BASE_SUFFIX = @"/v1.3/MobileApiHost/";
 #ifdef DEBUG
-const static NSString *SVC_BASE_HOST = @"10.177.206.34";
+const static NSString *SVC_BASE_HOST = @"223.4.20.20";
 const static NSString *SVC_BASE_SCHEMA = @"http";
-const static NSString *SVC_BASE_SUFFIX = @"/Mobile/";
+const static NSString *SVC_BASE_SUFFIX = @"/v1.3.0.8/MobileApiHost/";
 #endif
 
 #ifdef DailyBuild
