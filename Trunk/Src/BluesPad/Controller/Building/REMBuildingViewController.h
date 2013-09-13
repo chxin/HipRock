da2bfd288be14e73e8a818415d8baae64e20dbae
tantan <tan-tan.tan@schneider-electric.com>
2013-09-13 15:22:14 +0800

    fix bug #3344

--- Trunk/Src/BluesPad/Controller/Building/REMBuildingViewController.h
+++ Trunk/Src/BluesPad/Controller/Building/REMBuildingViewController.h
@@ -20,5 +20,5 @@
 @property (nonatomic,strong) REMSplashScreenController *splashScreenController;
 
 @property (nonatomic) CGFloat currentScrollOffset;
-- (void)notifyCustomImageLoaded;
+- (void)notifyCustomImageLoaded:(NSNumber *)buildingId;
 @end
