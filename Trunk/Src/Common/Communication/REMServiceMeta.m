f6915432965f0ffa939c26c564faa9c0e10a0876
张 锋 <zhangfeng0408@gmail.com>
2013-09-23 13:03:35 +0800

    change service url to 0.3.0.16

--- Trunk/Src/Common/Communication/REMServiceMeta.m
+++ Trunk/Src/Common/Communication/REMServiceMeta.m
@@ -20,19 +20,19 @@
 //const static NSString *SVC_BASE_SCHEMA = @"http";
 //const static NSString *SVC_BASE_SUFFIX = @"/Mobile/";
 
-//const static NSString *SVC_BASE_HOST = @"223.4.20.20";
-//const static NSString *SVC_BASE_SCHEMA = @"http";
-//const static NSString *SVC_BASE_SUFFIX = @"/v1.3.0.13/MobileApiHost/";
-
-const static NSString *SVC_BASE_HOST = @"admin.energy-here.com";
+const static NSString *SVC_BASE_HOST = @"223.4.20.20";
 const static NSString *SVC_BASE_SCHEMA = @"http";
-const static NSString *SVC_BASE_SUFFIX = @"/MobileApiHost/";
+const static NSString *SVC_BASE_SUFFIX = @"/v1.3.0.16/MobileApiHost/";
+
+//const static NSString *SVC_BASE_HOST = @"admin.energy-here.com";
+//const static NSString *SVC_BASE_SCHEMA = @"http";
+//const static NSString *SVC_BASE_SUFFIX = @"/MobileApiHost/";
 #endif
 
 #ifdef DailyBuild
 const static NSString *SVC_BASE_HOST = @"223.4.20.20";
 const static NSString *SVC_BASE_SCHEMA = @"http";
-const static NSString *SVC_BASE_SUFFIX = @"/v1.3.0.13/MobileApiHost/";
+const static NSString *SVC_BASE_SUFFIX = @"/v1.3.0.16/MobileApiHost/";
 #endif
 
 #ifdef InternalRelease
@@ -41,7 +41,7 @@ const static NSString *SVC_BASE_SUFFIX = @"/v1.3.0.13/MobileApiHost/";
 //const static NSString *SVC_BASE_SUFFIX = @"/MobileApiHost/";
 const static NSString *SVC_BASE_HOST = @"223.4.20.20";
 const static NSString *SVC_BASE_SCHEMA = @"http";
-const static NSString *SVC_BASE_SUFFIX = @"/v1.3.0.13/MobileApiHost/";
+const static NSString *SVC_BASE_SUFFIX = @"/v1.3.0.16/MobileApiHost/";
 #endif
 
 
