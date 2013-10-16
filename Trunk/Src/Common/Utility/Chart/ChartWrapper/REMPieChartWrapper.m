5ce8b400852682a7951a74e584c07afb438ddcb8
Zilong-Oscar.Xu <zilong@Oscars-MacBook-Pro.local>
2013-10-16 11:17:09 +0800

    PieChart bug fixed

--- Trunk/Src/Common/Utility/Chart/ChartWrapper/REMPieChartWrapper.m
+++ Trunk/Src/Common/Utility/Chart/ChartWrapper/REMPieChartWrapper.m
@@ -9,15 +9,20 @@
 #import "REMPieChartWrapper.h"
 
 @implementation REMPieChartWrapper
-
--(REMPieChartView*)renderContentView:(CGRect)frame data:(REMEnergyViewData*)energyViewData widgetContext:(REMWidgetContentSyntax*) widgetSyntax  status:(REMWidgetStatus)status {
+-(REMPieChartView*)renderContentView:(CGRect)frame data:(REMEnergyViewData*)energyViewData widgetContext:(REMWidgetContentSyntax*) widgetSyntax {
     REMChartConfig* chartConfig = [REMChartConfig getMinimunWidgetDefaultSetting];
     NSMutableArray* seriesArray = [[NSMutableArray alloc]init];
-    int seriesCount = 0;
-    if (energyViewData.targetEnergyData != nil && energyViewData.targetEnergyData != NULL) seriesCount =energyViewData.targetEnergyData.count;
     
-    REMTargetEnergyData* seriesData = [energyViewData.targetEnergyData objectAtIndex:0];
-    [seriesArray addObject: [[REMPieChartSeries alloc]initWithData:seriesData.energyData dataProcessor:self.dataProcessor plotStyle:nil]];
+    NSMutableArray* series0Data = [[NSMutableArray alloc]init];
+    int seriesCount = 0;
+    if (energyViewData != nil && energyViewData.targetEnergyData != NULL) seriesCount =energyViewData.targetEnergyData.count;
+    for (uint i = 0; i < seriesCount; i++) {
+        REMTargetEnergyData* seriesData = [energyViewData.targetEnergyData objectAtIndex:i];
+        if (seriesData.energyData != nil && seriesData.energyData.count > 0) {
+            [series0Data addObjectsFromArray:seriesData.energyData];
+        }
+    }
+    [seriesArray addObject: [[REMPieChartSeries alloc]initWithData:series0Data dataProcessor:self.dataProcessor plotStyle:nil]];
     
     chartConfig.series = seriesArray;
     return  [[REMPieChartView alloc]initWithFrame:frame chartConfig:chartConfig];
