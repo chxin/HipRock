1e058f751dcee6c9719dadc9e674765cd83baf35
Zilong-Oscar.Xu <zilong@Oscars-MacBook-Pro.local>
2013-10-17 17:31:05 +0800

    ScreenEdge gesture updated

--- Trunk/Src/BluesPad/View/Controls/REMScreenEdgetGestureRecognizer.m
+++ Trunk/Src/BluesPad/View/Controls/REMScreenEdgetGestureRecognizer.m
@@ -9,11 +9,15 @@
 #import "REMScreenEdgetGestureRecognizer.h"
 #import <UIKit/UIGestureRecognizerSubclass.h>
 
-@implementation REMScreenEdgetGestureRecognizer
+@implementation REMScreenEdgetGestureRecognizer {
+    CGPoint eventStartPoint;
+    CGPoint eventEndPoint;
+}
 
 - (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
     [super touchesBegan:touches withEvent:event];
-    float touchX = [self locationInView:self.view].x;
+    eventStartPoint = [self locationInView:self.view];
+    float touchX = eventStartPoint.x;
     if (touchX > 974 || touchX < 50) {
         self.state = UIGestureRecognizerStateBegan;
     } else {
@@ -22,8 +26,15 @@
 }
 - (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
     [super touchesEnded:touches withEvent:event];
-    
-    self.state = UIGestureRecognizerStateEnded;
-    NSLog(@"touchedEnded:%f",[self locationInView:self.view].x);
+    if (self.state == UIGestureRecognizerStateBegan) {
+        eventEndPoint = [self locationInView:self.view];
+        if (abs(eventEndPoint.x - eventStartPoint.x) > 100) {
+            self.state = UIGestureRecognizerStateEnded;
+        } else {
+            self.state = UIGestureRecognizerStateCancelled;
+        }
+    } else {
+        self.state = UIGestureRecognizerStateCancelled;
+    }
 }
 @end
