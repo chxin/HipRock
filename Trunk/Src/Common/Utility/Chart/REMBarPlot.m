874c241f3c62a98b2401f2eb210ecefb353246e2
Zilong-Oscar.Xu <zilong@Oscars-MacBook-Pro.local>
2013-11-01 13:07:12 +0800

    Temp check in for improve Coreplot

--- Trunk/Src/Common/Utility/Chart/REMBarPlot.m
+++ Trunk/Src/Common/Utility/Chart/REMBarPlot.m
@@ -23,13 +23,99 @@
  **/
 @implementation REMBarPlot {
     NSMutableDictionary* pathDic;
+    CGFloat unitLengthInView;
+}
+
+-(id)init {
+    return [super init];
+}
+
+-(id)initWithFrame:(CGRect)newFrame {
+    return [super initWithFrame:newFrame];
 }
 
 /// @cond
 
--(CGFloat)lengthInView:(NSDecimal)decimalLength
+-(BOOL)barAtRecordIndex:(NSUInteger)idx basePoint:(CGPoint *)basePoint tipPoint:(CGPoint *)tipPoint
 {
-    CGFloat length;
+    BOOL horizontalBars            = self.barsAreHorizontal;
+    CPTCoordinate independentCoord = (horizontalBars ? CPTCoordinateY : CPTCoordinateX);
+    CPTCoordinate dependentCoord   = (horizontalBars ? CPTCoordinateX : CPTCoordinateY);
+    
+    CPTPlotSpace *thePlotSpace = self.plotSpace;
+    
+    if ( self.doublePrecisionCache ) {
+        double plotPoint[2];
+        plotPoint[independentCoord] = [self cachedDoubleForField:CPTBarPlotFieldBarLocation recordIndex:idx];
+        if ( isnan(plotPoint[independentCoord]) ) {
+            return NO;
+        }
+        
+        // Tip point
+        plotPoint[dependentCoord] = [self cachedDoubleForField:CPTBarPlotFieldBarTip recordIndex:idx];
+        if ( isnan(plotPoint[dependentCoord]) ) {
+            return NO;
+        }
+        *tipPoint = [thePlotSpace plotAreaViewPointForDoublePrecisionPlotPoint:plotPoint];
+        
+        // Base point
+        if ( !self.barBasesVary ) {
+            plotPoint[dependentCoord] = CPTDecimalDoubleValue(self.baseValue);
+        }
+        else {
+            plotPoint[dependentCoord] = [self cachedDoubleForField:CPTBarPlotFieldBarBase recordIndex:idx];
+        }
+        if ( isnan(plotPoint[dependentCoord]) ) {
+            return NO;
+        }
+        *basePoint = [thePlotSpace plotAreaViewPointForDoublePrecisionPlotPoint:plotPoint];
+    }
+    else {
+        NSDecimal plotPoint[2];
+        plotPoint[independentCoord] = [self cachedDecimalForField:CPTBarPlotFieldBarLocation recordIndex:idx];
+        if ( NSDecimalIsNotANumber(&plotPoint[independentCoord]) ) {
+            return NO;
+        }
+        
+        // Tip point
+        plotPoint[dependentCoord] = [self cachedDecimalForField:CPTBarPlotFieldBarTip recordIndex:idx];
+        if ( NSDecimalIsNotANumber(&plotPoint[dependentCoord]) ) {
+            return NO;
+        }
+        *tipPoint = [thePlotSpace plotAreaViewPointForPlotPoint:plotPoint];
+        
+        // Base point
+        if ( !self.barBasesVary ) {
+            plotPoint[dependentCoord] = self.baseValue;
+        }
+        else {
+            plotPoint[dependentCoord] = [self cachedDecimalForField:CPTBarPlotFieldBarBase recordIndex:idx];
+        }
+        if ( NSDecimalIsNotANumber(&plotPoint[dependentCoord]) ) {
+            return NO;
+        }
+        *basePoint = [thePlotSpace plotAreaViewPointForPlotPoint:plotPoint];
+    }
+    
+    // Determine bar width and offset.
+    CGFloat barOffsetLength = [self lengthInView:self.barOffset recal:NO] * self.barOffsetScale;
+    
+    // Offset
+    if ( horizontalBars ) {
+        basePoint->y += barOffsetLength;
+        tipPoint->y  += barOffsetLength;
+    }
+    else {
+        basePoint->x += barOffsetLength;
+        tipPoint->x  += barOffsetLength;
+    }
+    
+    return YES;
+}
+-(CGFloat)lengthInView:(NSDecimal)decimalLength recal:(BOOL)recal
+{
+    if (!recal) return unitLengthInView * [NSDecimalNumber decimalNumberWithDecimal:decimalLength].floatValue;
+    CGFloat length = 0.0;
     
     if ( self.barWidthsAreInViewCoordinates ) {
         length = CPTDecimalCGFloatValue(decimalLength);
@@ -81,6 +167,239 @@
     }
     return length;
 }
+-(void)drawInContext:(CGContextRef)ctx {
+    unitLengthInView = [self lengthInView:[NSDecimalNumber numberWithInt:1].decimalValue recal:YES];
+    [super drawInContext:ctx];
+}
+-(void)setFrame:(CGRect)frame {
+    [super setFrame:frame];
+}
+-(void)renderAsVectorInContext:(CGContextRef)context
+{
+    self.masksToBorder = NO;
+    if ( self.hidden ) {
+        return;
+    }
+    
+    CPTMutableNumericData *cachedLocations = [self cachedNumbersForField:CPTBarPlotFieldBarLocation];
+    CPTMutableNumericData *cachedLengths   = [self cachedNumbersForField:CPTBarPlotFieldBarTip];
+    if ( (cachedLocations == nil) || (cachedLengths == nil) ) {
+        return;
+    }
+    
+    BOOL basesVary                     = self.barBasesVary;
+    CPTMutableNumericData *cachedBases = [self cachedNumbersForField:CPTBarPlotFieldBarBase];
+    if ( basesVary && (cachedBases == nil) ) {
+        return;
+    }
+    
+    NSUInteger barCount = self.cachedDataCount;
+    if ( barCount == 0 ) {
+        return;
+    }
+    
+    if ( cachedLocations.numberOfSamples != cachedLengths.numberOfSamples ) {
+        [NSException raise:CPTException format:@"Number of bar locations and lengths do not match"];
+    }
+    
+    if ( basesVary && (cachedLengths.numberOfSamples != cachedBases.numberOfSamples) ) {
+        [NSException raise:CPTException format:@"Number of bar lengths and bases do not match"];
+    }
+    
+    // This is where subclasses do their drawing
+//    if ( self.renderingRecursively ) {
+//        [self applyMaskToContext:context];
+//    }
+//    [self.shadow setShadowInContext:context];
+//    [super renderAsVectorInContext:context];
+    
+    CGContextBeginTransparencyLayer(context, NULL);
+//    for ( NSUInteger ii = 0; ii < barCount; ii++ ) {
+//        // Draw
+//        [self drawBarInContext:context recordIndex:ii];
+//    }
+    
+    CGMutablePathRef path = CGPathCreateMutable();
+    for ( NSUInteger ii = 0; ii < barCount; ii++ ) {
+        // Draw
+        CGPoint basePoint, tipPoint;
+        BOOL barExists = [self barAtRecordIndex:ii basePoint:&basePoint tipPoint:&tipPoint];
+        
+        if ( !barExists ) {
+            continue;
+        }
+        
+        if (![self barIsVisibleWithIndex:ii]) continue;
+        CGRect r = [self getRectOfBasePoint:basePoint tipPoint:tipPoint];
+        CGPathAddRect(path, Nil, r);
+    }
+    CGContextSaveGState(context);
+//    CPTFill *theBarFill = [self barFillForIndex:0];
+    CGContextBeginPath(context);
+    CGContextAddPath(context, path);
+    CGContextSetFillColorWithColor(context, [UIColor redColor].CGColor);
+    CGContextFillPath(context);
+    
+//    if ( [theBarFill isKindOfClass:[CPTFill class]] ) {
+//        [theBarFill fillPathInContext:context];
+//    }
+    CGContextRestoreGState(context);
+    CGPathRelease(path);
+
+
+    CGContextEndTransparencyLayer(context);
+//    CGMutablePathRef path = CGPathCreateMutable();
+//    CGPathAddRect(path, NULL, CGRectMake(-25, 0, 50, 50));
+//    CGContextSaveGState(context);
+//    CPTFill *theBarFill = [self barFillForIndex:0];
+//    if ( [theBarFill isKindOfClass:[CPTFill class]] ) {
+//        CGContextBeginPath(context);
+//        CGContextAddPath(context, path);
+//        [theBarFill fillPathInContext:context];
+//    }
+//    CGContextRestoreGState(context);
+//    CGPathRelease(path);
+//    
+//    CGContextEndTransparencyLayer(context);
+////    self.paddingLeft = 25;
+//    CALayer* superLayer = self.superlayer;
+//    while (superLayer && [superLayer isKindOfClass:[CPTLayer class]]) {
+//        superLayer.masksToBounds = NO;
+//        CGRect FFF = superLayer.frame;
+////        superLayer.frame = CGRectMake(-300, 0, superLayer.frame.size.width+300, superLayer.frame.size.height);
+////        superLayer.backgroundColor = [UIColor yellowColor].CGColor;
+//        superLayer.borderWidth =1.0f;
+//        if ([superLayer isKindOfClass:[CPTPlotGroup class]]) {
+//            superLayer.borderColor = [UIColor greenColor].CGColor;
+//        } else if ([superLayer isKindOfClass:[CPTPlotArea class]]) {
+//            superLayer.borderColor = [UIColor blueColor].CGColor;
+//        } else if ([superLayer isKindOfClass:[CPTPlotAreaFrame class]]) {
+//            superLayer.borderColor = [UIColor orangeColor].CGColor;
+//            superLayer.borderWidth = 2.0f;
+//        } else {
+//            
+//        }
+//        superLayer = superLayer.superlayer;
+//    }
+//    self.backgroundColor = [UIColor redColor].CGColor;
+}
+-(void)drawLayer:(CALayer *)layer inContext:(CGContextRef)ctx {
+    
+}
+
+-(BOOL)barIsVisibleWithBasePoint:(CGPoint)basePoint
+{
+//    return YES;
+    BOOL horizontalBars    = self.barsAreHorizontal;
+    CGFloat barWidthLength = [self lengthInView:self.barWidth recal:NO] * self.barWidthScale;
+    CGFloat halfBarWidth   = CPTFloat(0.5) * barWidthLength;
+    CPTPlotArea *thePlotArea = self.plotArea;
+
+    CGFloat lowerBound = ( horizontalBars ? CGRectGetMinY(thePlotArea.bounds) : CGRectGetMinX(thePlotArea.bounds) );
+    CGFloat upperBound = ( horizontalBars ? CGRectGetMaxY(thePlotArea.bounds) : CGRectGetMaxX(thePlotArea.bounds) );
+    CGFloat base       = (horizontalBars ? basePoint.y : basePoint.x);
+    
+    return (base + halfBarWidth >= lowerBound) && (base - halfBarWidth <= upperBound);
+}
+-(BOOL)barIsVisibleWithIndex:(NSUInteger)index
+{
+    CPTXYPlotSpace* p = (CPTXYPlotSpace*)self.plotSpace;
+    CPTPlotRange* xRange = p.xRange;
+    return [xRange contains:[NSDecimalNumber numberWithUnsignedInteger:index].decimalValue];
+}
+-(void)drawBarInContext:(CGContextRef)context recordIndex:(NSUInteger)idx
+{
+    // Get base and tip points
+    CGPoint basePoint, tipPoint;
+    BOOL barExists = [self barAtRecordIndex:idx basePoint:&basePoint tipPoint:&tipPoint];
+    
+    if ( !barExists ) {
+        return;
+    }
+    
+    // Return if bar is off screen
+//    if ( ![self barIsVisibleWithBasePoint:basePoint] ) {
+//        return;
+//    }
+    if (![self barIsVisibleWithIndex:idx]) return;
+    
+    
+    CGMutablePathRef path = [self newBarPathWithContext:context basePoint:basePoint tipPoint:tipPoint];
+    
+    if ( path ) {
+        CGContextSaveGState(context);
+        
+        CPTFill *theBarFill = [self barFillForIndex:idx];
+        if ( [theBarFill isKindOfClass:[CPTFill class]] ) {
+            CGContextBeginPath(context);
+            CGContextAddPath(context, path);
+            [theBarFill fillPathInContext:context];
+        }
+        
+        CPTLineStyle *theLineStyle = [self barLineStyleForIndex:idx];
+        if ( [theLineStyle isKindOfClass:[CPTLineStyle class]] ) {
+            CGContextBeginPath(context);
+            CGContextAddPath(context, path);
+            [theLineStyle setLineStyleInContext:context];
+            [theLineStyle strokePathInContext:context];
+        }
+        
+        CGContextRestoreGState(context);
+        
+        CGPathRelease(path);
+    }
+}
+-(CPTFill *)barFillForIndex:(NSUInteger)idx
+{
+    CPTFill *theBarFill = [self cachedValueForKey:CPTBarPlotBindingBarFills recordIndex:idx];
+    
+    if ( (theBarFill == nil) || (theBarFill == [CPTPlot nilData]) ) {
+        theBarFill = self.fill;
+    }
+    
+    return theBarFill;
+}
+
+-(CPTLineStyle *)barLineStyleForIndex:(NSUInteger)idx
+{
+    CPTLineStyle *theBarLineStyle = [self cachedValueForKey:CPTBarPlotBindingBarLineStyles recordIndex:idx];
+    
+    if ( (theBarLineStyle == nil) || (theBarLineStyle == [CPTPlot nilData]) ) {
+        theBarLineStyle = self.lineStyle;
+    }
+    
+    return theBarLineStyle;
+}
+
+-(CGRect)getRectOfBasePoint:(CGPoint)basePoint tipPoint:(CGPoint)tipPoint {
+    BOOL horizontalBars = self.barsAreHorizontal;
+    
+    CGFloat barWidthLength = [self lengthInView:self.barWidth recal:NO] * self.barWidthScale;
+    CGFloat halfBarWidth   = CPTFloat(0.5) * barWidthLength;
+    
+    CGRect barRect;
+    
+    if ( horizontalBars ) {
+        barRect = CPTRectMake(basePoint.x, basePoint.y - halfBarWidth, tipPoint.x - basePoint.x, barWidthLength);
+    }
+    else {
+        barRect = CPTRectMake(basePoint.x - halfBarWidth, basePoint.y, barWidthLength, tipPoint.y - basePoint.y);
+    }
+    
+    int widthNegative  = signbit(barRect.size.width);
+    int heightNegative = signbit(barRect.size.height);
+    
+    
+    if ( widthNegative && (barRect.size.width > 0.0) ) {
+        barRect.origin.x  += barRect.size.width;
+        barRect.size.width = -barRect.size.width;
+    }
+    if ( heightNegative && (barRect.size.height > 0.0) ) {
+        barRect.origin.y   += barRect.size.height;
+        barRect.size.height = -barRect.size.height;
+    }
+    return barRect;
+}
 
 -(CGMutablePathRef)newBarPathWithContext:(CGContextRef)context basePoint:(CGPoint)basePoint tipPoint:(CGPoint)tipPoint
 {
@@ -91,7 +410,7 @@
     // drawing a bar and for doing hit-testing on a click/touch event
     BOOL horizontalBars = self.barsAreHorizontal;
     
-    CGFloat barWidthLength = [self lengthInView:self.barWidth] * self.barWidthScale;
+    CGFloat barWidthLength = [self lengthInView:self.barWidth recal:NO] * self.barWidthScale;
     CGFloat halfBarWidth   = CPTFloat(0.5) * barWidthLength;
     
     CGRect barRect;
@@ -202,7 +521,6 @@
             CGPathCloseSubpath(path);
         }
     }
-//    [pathDic setObject:[NSValue valueWithPointer:path] forKey:@(tipPoint.x)];
     
     return path;
 }
