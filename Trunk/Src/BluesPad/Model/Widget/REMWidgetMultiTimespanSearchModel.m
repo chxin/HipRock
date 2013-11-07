2186c4d8a92a0d4e41d9c9deeb9b313960515478
tantan <tan-tan.tan@schneider-electric.com>
2013-11-07 14:03:18 +0800

    multi time span

--- Trunk/Src/BluesPad/Model/Widget/REMWidgetMultiTimespanSearchModel.m
+++ Trunk/Src/BluesPad/Model/Widget/REMWidgetMultiTimespanSearchModel.m
@@ -12,6 +12,25 @@
 
 - (void)setTimeRangeItem:(REMTimeRange *)range AtIndex:(NSUInteger)index
 {
+    REMTimeRange *oldRange=self.timeRangeArray[0];
+
+    if([oldRange.startTime isEqualToDate:range.startTime]==NO){
+        NSMutableArray *newArray=[[NSMutableArray alloc]initWithCapacity:self.timeRangeArray.count];
+        NSTimeInterval elapsed=[range.endTime timeIntervalSinceDate:range.startTime];
+        NSTimeInterval startGap=[range.startTime timeIntervalSinceDate:oldRange.startTime];
+        
+        for (int i=0; i<self.timeRangeArray.count; ++i) {
+            REMTimeRange *r=self.timeRangeArray[i];
+            NSDate *newStart= [r.startTime dateByAddingTimeInterval:startGap];
+            NSDate *newEnd=[newStart dateByAddingTimeInterval:elapsed];
+            REMTimeRange *newR=[[REMTimeRange alloc]initWithStartTime:newStart EndTime:newEnd];
+            [newArray addObject:newR];
+        }
+        self.timeRangeArray=newArray;
+        
+    }
+    
+    
     
 }
 
