cb88b45fd6989bd8aeb9c56abbf21a5239d0111b
Zilong-Oscar.Xu <zilong@Oscars-MacBook-Pro.local>
2013-11-13 15:13:27 +0800

    calendar ok

--- Trunk/Src/Common/Utility/Chart/ChartWrapper/TrendChartWrapper/REMTrendWidgetWrapper.m
+++ Trunk/Src/Common/Utility/Chart/ChartWrapper/TrendChartWrapper/REMTrendWidgetWrapper.m
@@ -14,6 +14,13 @@
 
 @implementation REMTrendWidgetWrapper
 
+-(REMAbstractChartWrapper*)initWithFrame:(CGRect)frame data:(REMEnergyViewData*)energyViewData widgetContext:(REMWidgetContentSyntax*) widgetSyntax  style:(REMChartStyle*)style{
+    self = [super initWithFrame:frame data:energyViewData widgetContext:widgetSyntax style:style];
+    if (self) {
+        _calenderType = REMCalendarTypeNone;
+    }
+    return self;
+}
 -(NSArray*)extraSeriesConfig {
     NSMutableArray* seriesArray = [[NSMutableArray alloc]init];
     uint seriesCount = [self getSeriesCount];
@@ -113,6 +120,7 @@
     REMTrendChartView* myView = [[REMTrendChartView alloc]initWithFrame:frame chartConfig:chartConfig];
     NSRange initialRange = [self createInitialRange];
     [myView renderRange:initialRange.location length:initialRange.length];
+    
     return  myView;
 }
 
@@ -135,5 +143,40 @@
     chartConfig.xAxisConfig.labelFormatter = [[REMXFormatter alloc]initWithStartDate:self.baseDateOfX dataStep:chartConfig.step interval:0];
     
     return chartConfig;
+}
+
+-(void)setCalenderType:(REMCalendarType)calenderType {
+    if (calenderType == self.calenderType) return;
+    _calenderType = calenderType;
+    CPTXYAxis* xAxis = ((CPTXYAxisSet*)((REMTrendChartView*)self.view).hostedGraph.axisSet).xAxis;
+    
+    if(self.calenderType == REMCalendarTypeNone) {
+        if (xAxis.backgroundLimitBands != nil) {
+            while (xAxis.backgroundLimitBands.count != 0) {
+                [xAxis removeBackgroundLimitBand:xAxis.backgroundLimitBands[0]];
+            }
+        }
+    } else {
+        for (REMEnergyCalendarData* calender in self.energyViewData.calendarData) {
+            CPTColor* fillColor = nil;
+            if (self.calenderType == REMCalendarTypeHCSeason) {
+                if (calender.calendarType == REMCalenderTypeHeatSeason) {
+                    fillColor = [CPTColor colorWithCGColor:[REMColor colorByHexString:@"#fcf0e4" alpha:0.5].CGColor];
+                } else if (calender.calendarType == REMCalenderTypeCoolSeason) {
+                    fillColor = [CPTColor colorWithCGColor:[REMColor colorByHexString:@"#e3f0ff" alpha:0.5].CGColor];
+                }
+            } else if (self.calenderType == REMCalenderTypeHoliday) {
+                if (calender.calendarType == REMCalenderTypeWorkDay) {
+                    fillColor = [CPTColor colorWithCGColor:[REMColor colorByHexString:@"#eaeaea" alpha:0.5].CGColor];
+                }
+            }
+            if (fillColor == nil) continue;
+            for (REMTimeRange* range in calender.timeRanges) {
+                NSNumber* start = [sharedProcessor processX:range.startTime];
+                CPTLimitBand* b = [[CPTLimitBand alloc]initWithRange:[[CPTPlotRange alloc] initWithLocation:start.decimalValue length:CPTDecimalFromDouble([sharedProcessor processX:range.endTime].doubleValue - start.doubleValue)] fill:[CPTFill fillWithColor:fillColor]];
+                [xAxis addBackgroundLimitBand:b];
+            }
+        }
+    }
 }
 @end
--- Trunk/Src/Common/Utility/REMColor.m
+++ Trunk/Src/Common/Utility/REMColor.m
@@ -35,14 +35,19 @@ static NSArray *chartColor;
 
 + (UIColor *)colorByHexString:(NSString *)hexString
 {
+    return [self colorByHexString:hexString alpha:1.0];
+}
+
++ (UIColor *)colorByHexString:(NSString *)hexString alpha:(CGFloat)alpha
+{
     unsigned rgbValue=0;
     NSScanner *scanner = [NSScanner scannerWithString:hexString];
     [scanner setScanLocation:1];
     [scanner scanHexInt:&rgbValue];
     return [UIColor colorWithRed:
             ((rgbValue & 0xFF0000)>>16)/255.00
-                                     green:((rgbValue & 0xFF00) >> 8)/255.00
-                                      blue:((rgbValue & 0xFF)/255.00) alpha:1.0];
+                           green:((rgbValue & 0xFF00) >> 8)/255.00
+                            blue:((rgbValue & 0xFF)/255.00) alpha:alpha];
     
     
 }
