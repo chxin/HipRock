2788b43d93dbd96bb961bf9405925028d50b5fa6
tantan <tan-tan.tan@schneider-electric.com>
2013-11-20 09:22:48 +0800

    commit

--- Trunk/Src/BluesPad/Controller/Splash/REMSplashScreenController.m
+++ Trunk/Src/BluesPad/Controller/Splash/REMSplashScreenController.m
@@ -110,38 +110,38 @@
     self.navigationController.navigationBarHidden = YES;
     //    [self.view addSubview:[[REMTrend alloc]initWithFrame:CGRectMake(100, 0, 924, 708)]];
     
-    [self oscarTest];
+    //[self oscarTest];
     
     
     //decide where to go
-//    [self recoverAppContext];
-//    
-//    if([self isAlreadyLogin]){
-//        [self breathAnimation:^(void){
-//            [self breathAnimation:nil];
-//            
-//            SEL selector = @selector(breathAnimation:);
-//            
-//            NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:[[self class] instanceMethodSignatureForSelector:selector]];
-//            [invocation setTarget:self];
-//            [invocation setSelector:selector];
-//            
-//            NSTimer *timer = [NSTimer scheduledTimerWithTimeInterval:3.0 invocation:invocation repeats:YES];
-//            
-//            [self showMapView:^(void){
-//                if(timer != nil){
-//                    if([timer isValid])
-//                        [timer invalidate];
-//                }
-//            }];
-//        }];
-//    }
-//    else{
-//        [self breathAnimation:^(void){
-//            [self.logoView setHidden:YES];
-//            [self showLoginView:YES];
-//        }];
-//    }
+    [self recoverAppContext];
+    
+    if([self isAlreadyLogin]){
+        [self breathAnimation:^(void){
+            [self breathAnimation:nil];
+            
+            SEL selector = @selector(breathAnimation:);
+            
+            NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:[[self class] instanceMethodSignatureForSelector:selector]];
+            [invocation setTarget:self];
+            [invocation setSelector:selector];
+            
+            NSTimer *timer = [NSTimer scheduledTimerWithTimeInterval:3.0 invocation:invocation repeats:YES];
+            
+            [self showMapView:^(void){
+                if(timer != nil){
+                    if([timer isValid])
+                        [timer invalidate];
+                }
+            }];
+        }];
+    }
+    else{
+        [self breathAnimation:^(void){
+            [self.logoView setHidden:YES];
+            [self showLoginView:YES];
+        }];
+    }
 }
 
 -(void)oscarTest {
