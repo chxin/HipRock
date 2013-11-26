5b3591b97bb5d7337139a5585692395198bfbc73
zilong-oscar.xu <zilong-oscar.xu@schneider-electric.com>
2013-11-26 17:24:18 +0800

    GUI

---
+++ Trunk/Src/Common/Utility/Chart/ChartView/REMPieShadowLayer.h
@@ -0,0 +1,14 @@
+//
+//  REMPieShadowLayer.h
+//  Blues
+//
+//  Created by Zilong-Oscar.Xu on 11/26/13.
+//
+//
+
+#import <QuartzCore/QuartzCore.h>
+
+@interface REMPieShadowLayer : CALayer
+
+@property (nonatomic, assign) CGFloat shadowRadius;
+@end
