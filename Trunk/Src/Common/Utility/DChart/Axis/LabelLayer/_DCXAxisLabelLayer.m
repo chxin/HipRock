e8764dafa59b888025c6191beec9a44ddc8ac28e
zilong-oscar.xu <zilong-oscar.xu@schneider-electric.com>
2013-12-11 14:07:33 +0800

    improve x label performance

--- Trunk/Src/Common/Utility/DChart/Axis/LabelLayer/_DCXAxisLabelLayer.m
+++ Trunk/Src/Common/Utility/DChart/Axis/LabelLayer/_DCXAxisLabelLayer.m
@@ -11,9 +11,9 @@
 #import "_DCXLabelFormatter.h"
 
 @interface _DCXAxisLabelLayer()
-//@property (nonatomic, assign) CGFloat fontSize;
-//@property (nonatomic, assign) CTFontRef fontRef;
-@property (nonatomic, strong) _DCLayerTrashbox* trashbox;
+
+@property (nonatomic, strong) CATextLayer* cacheLayer;
+@property (nonatomic, strong) NSMutableDictionary* visableLabelLayers;
 
 @end
 
@@ -24,7 +24,7 @@
     self = [super initWithContext:context];
     if (self) {
         self.backgroundColor = [UIColor clearColor].CGColor;
-        self.trashbox = [[_DCLayerTrashbox alloc]init];
+        self.visableLabelLayers = [[NSMutableDictionary alloc]init];
     }
     return self;
 }
@@ -45,19 +45,91 @@
     CGContextSetLineWidth(ctx, self.axis.lineWidth);
     CGContextSetStrokeColorWithColor(ctx, self.axis.lineColor.CGColor);
     CGContextStrokePath(ctx);
-    
-    for (NSNumber* key in self.trashbox.xToLayerDic.allKeys) {
-        CATextLayer* text = self.trashbox.xToLayerDic[key];
-        [text removeFromSuperlayer];
+    [self updateTexts];
+}
+
+-(void)updateTexts {
+    BOOL caTransationState = CATransaction.disableActions;
+    [CATransaction setDisableActions:YES];
+    if (REMIsNilOrNull(self.cacheLayer)) {
+        self.cacheLayer = [self constructCacheTextLayer];
     }
-    [self.trashbox.xToLayerDic removeAllObjects];
+    CGFloat maxLabelLength = INT32_MAX;
+    if (self.labelFormatter && [self.labelFormatter respondsToSelector:@selector(getMaxXLabelLengthIn:)]) {
+        maxLabelLength = [((id<_DCXLabelFormatterProtocal>)self.labelFormatter) getMaxXLabelLengthIn:self.bounds];
+    }
+    CGFloat offset = 0;
+    if (!self.graphContext.xLabelAlignToTick) {
+        offset = 0.5;
+    }
+    
     int start = floor(self.graphContext.hRange.location);
     int end = ceil(self.graphContext.hRange.end);
+    NSMutableArray* textFrameArray = [[NSMutableArray alloc]init];
+    NSMutableArray* textStringArray = [[NSMutableArray alloc]init];
+    NSMutableArray* textXValArray = [[NSMutableArray alloc]init];
     for (int i = start; i <= end; i++) {
-        if (self.trashbox.xToLayerDic[@(i)] == nil) {
-            [self addLabelForX:i];
+        NSString* labelText = [self textForX:i];
+        if (REMIsNilOrNull(labelText) || labelText.length == 0) continue;
+        CGFloat centerX = (i + offset - self.graphContext.hRange.location) * self.frame.size.width / self.graphContext.hRange.length;
+        CGSize size = [DCUtility getSizeOfText:labelText forFont:self.font];
+        CGRect textFrame;
+        if (size.width > maxLabelLength) {
+            textFrame = CGRectMake(centerX-maxLabelLength/2,self.frame.size.height-size.height, maxLabelLength,size.height);
+        } else {
+            textFrame = CGRectMake(centerX-size.width/2,self.frame.size.height-size.height, size.width,size.height);
+        }
+        if ([DCUtility isFrame:textFrame visableIn:self.bounds]) {
+            [textStringArray addObject:labelText];
+            [textFrameArray addObject:[NSValue valueWithCGRect:textFrame]];
+            [textXValArray addObject:@(i)];
         }
     }
+    
+    for (int i = 0; i < textStringArray.count; i++) {
+        CATextLayer* t = self.visableLabelLayers[textXValArray[i]];
+        if (REMIsNilOrNull(t)) {
+            t = self.cacheLayer;
+            t.hidden = NO;
+            [t setString:textStringArray[i]];
+            [self.visableLabelLayers setObject:t forKey:textXValArray[i]];
+            self.cacheLayer = [self constructCacheTextLayer];
+        }
+        CGRect rect;
+        [[textFrameArray objectAtIndex:i] getValue:&rect];
+        t.frame = rect;
+    }
+    
+    NSMutableArray* layersToBeRemove = [[NSMutableArray alloc]init];
+    NSArray* allkeys = self.visableLabelLayers.allKeys.copy;
+    for (NSNumber* key in allkeys) {
+        if (![textXValArray containsObject:key]) {
+            [layersToBeRemove addObject:key];
+        }
+    }
+    for (NSNumber* i in layersToBeRemove) {
+        [self.visableLabelLayers[i] removeFromSuperlayer];
+        [self.visableLabelLayers removeObjectForKey:i];
+    }
+    
+    [CATransaction setDisableActions:caTransationState];
+}
+
+-(CATextLayer*)constructCacheTextLayer {
+    CTFontRef fRef = CTFontCreateWithName((__bridge CFStringRef)self.font.fontName,
+                                          self.font.pointSize,
+                                          NULL);
+    CATextLayer*text = [[CATextLayer alloc]init];
+    text.font = fRef;
+    text.fontSize = self.font.pointSize;
+    text.contentsScale = [[UIScreen mainScreen] scale];
+    text.foregroundColor = self.fontColor.CGColor;
+    text.truncationMode = kCATruncationEnd;
+    text.alignmentMode = kCAAlignmentLeft;
+    text.hidden = YES;
+    [self addSublayer:text];
+    CFRelease(fRef);
+    return text;
 }
 
 -(void)willHRangeChanged:(DCRange *)oldRange newRange:(DCRange *)newRange {
@@ -66,40 +138,9 @@
 
 -(void)didHRangeChanged:(DCRange*)oldRange newRange:(DCRange*)newRange {
     if ([DCRange isRange:oldRange equalTo:newRange]) return;
-    
-    int start = floor(self.graphContext.hRange.location);
-    int end = ceil(self.graphContext.hRange.length+self.graphContext.hRange.location);
-    BOOL caTransationState = CATransaction.disableActions;
-    [CATransaction setDisableActions:YES];
-    if (oldRange.length == newRange.length) {
-        double xMovementInScreen = self.frame.size.width * (newRange.location - oldRange.location) / newRange.length;
-        
-        for (NSNumber* key in self.trashbox.xToLayerDic.allKeys) {
-            CATextLayer* text = self.trashbox.xToLayerDic[key];
-            CGRect toFrame = CGRectMake(text.frame.origin.x-xMovementInScreen, text.frame.origin.y, text.frame.size.width, text.frame.size.height);
-            if ([DCUtility isFrame:toFrame visableIn:self.bounds]) {
-                text.frame = toFrame;
-            } else {
-                [self.trashbox.xToLayerDic removeObjectForKey:key];
-                [text removeFromSuperlayer];
-            }
-        }
-    } else {
-        
-        for (NSNumber* key in self.trashbox.xToLayerDic.allKeys) {
-            CATextLayer* text = self.trashbox.xToLayerDic[key];
-            [self.trashbox.xToLayerDic removeObjectForKey:key];
-            [text removeFromSuperlayer];
-        }
-    }
-    
-    [CATransaction setDisableActions:caTransationState];
-    [self updateXFormatterInterval];
-    for (int i = start; i <= end; i++) {
-        if (self.trashbox.xToLayerDic[@(i)] == nil) {
-            [self addLabelForX:i];
-        }
-    }
+    if (oldRange.length != newRange.length)
+        [self updateXFormatterInterval];
+    [self updateTexts];
 }
 
 
@@ -108,55 +149,6 @@
         return [self.labelFormatter stringForObjectValue:@(x)];
     } else {
         return [NSString stringWithFormat:@"%i", x];
-    }
-}
-
--(void)addLabelForX:(NSInteger)x {
-//    if (self.fontRef == nil) {
-//        self.fontSize = self.font.pointSize;
-//        self.fontRef = CTFontCreateWithName((__bridge CFStringRef)self.font.fontName,
-//                                            self.fontSize,
-//                                            NULL);
-//    }
-    CGFloat offset = 0;
-    if (!self.graphContext.xLabelAlignToTick) {
-        offset = 0.5;
-    }
-    CGFloat centerX = (x + offset - self.graphContext.hRange.location) * self.frame.size.width / self.graphContext.hRange.length;
-    CGFloat maxLabelLength = INT32_MAX;
-    if (self.labelFormatter && [self.labelFormatter respondsToSelector:@selector(getMaxXLabelLengthIn:)]) {
-        maxLabelLength = [((id<_DCXLabelFormatterProtocal>)self.labelFormatter) getMaxXLabelLengthIn:self.bounds];
-    }
-    CATextLayer* text = (CATextLayer*)[self.trashbox popLayerFromTrashBox];
-    if (!text) {
-        CTFontRef fRef = CTFontCreateWithName((__bridge CFStringRef)self.font.fontName,
-                                              self.font.pointSize,
-                                              NULL);
-        text = [[CATextLayer alloc]init];
-        text.font = fRef;
-        text.fontSize = self.font.pointSize;
-        text.contentsScale = [[UIScreen mainScreen] scale];
-        text.foregroundColor = self.fontColor.CGColor;
-        text.alignmentMode = kCAAlignmentCenter;
-        text.truncationMode = kCATruncationEnd;
-        CFRelease(fRef);
-    }
-    NSString* labelText = [self textForX:x];
-    [text setString:labelText];
-    CGSize size = [DCUtility getSizeOfText:labelText forFont:self.font];
-    if (size.width > maxLabelLength) {
-        text.alignmentMode = kCAAlignmentLeft;
-        text.truncationMode = kCATruncationEnd;
-        text.frame = CGRectMake(centerX-maxLabelLength/2,self.frame.size.height-size.height, maxLabelLength,size.height);
-    } else {
-        text.alignmentMode = kCAAlignmentCenter;
-        text.truncationMode = kCATruncationNone;
-        text.frame = CGRectMake(centerX-size.width/2,self.frame.size.height-size.height, size.width,size.height);
-    }
-    if ([DCUtility isFrame:text.frame visableIn:self.bounds]) {
-        [self.trashbox.xToLayerDic setObject:text forKey:@(x)];
-        [self addSublayer:text];
-    } else {
     }
 }
 
