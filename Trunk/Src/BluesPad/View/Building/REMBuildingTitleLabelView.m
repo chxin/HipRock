3dd8b9cd87f4b3eb721412cd78524339a6b6bdd0
tantan <tan-tan.tan@schneider-electric.com>
2013-12-12 16:15:39 +0800

    add digit for airqulity

--- Trunk/Src/BluesPad/View/Building/REMBuildingTitleLabelView.m
+++ Trunk/Src/BluesPad/View/Building/REMBuildingTitleLabelView.m
@@ -63,7 +63,11 @@
     textLabel.backgroundColor=[UIColor clearColor];
     textLabel.shadowOffset=CGSizeMake(1, 1);
     textLabel.shadowColor=[UIColor colorWithRed:0 green:0 blue:0 alpha:0.2];
-    textLabel.text=[REMNumberHelper formatStringWithThousandSep:data.dataValue withRoundDigit:0 withIsRound:YES];
+    int digit=0;
+    if ([data.dataValue isGreaterThanOrEqualTo:@(1000)]==NO) {
+        digit=2;
+    }
+    textLabel.text=[REMNumberHelper formatStringWithThousandSep:data.dataValue withRoundDigit:digit withIsRound:YES];
     [self addSubview:textLabel];
     self.textLabel=textLabel;
     //NSLog(@"font:%@",[UIFont fontWithName:@"HelveticaNeue-UltraLight" size:valueSize]);
