622999104a5e48f2c927e58a278dcc275da7ad3c
张 锋 <zhangfeng0408@gmail.com>
2014-04-03 15:44:09 +0800

    added link for installing cerficate for ios 7.1

--- Build/BuildCenter/controllers/homecontroller.py
+++ Build/BuildCenter/controllers/homecontroller.py
@@ -9,20 +9,25 @@ t_globals = {
 }
 render = web.template.render('templates', base='master', globals=t_globals)
 
+def parse_globals():
+	uagent = web.ctx.env.get('HTTP_USER_AGENT')
+	if 'iPad; CPU OS 7_1' in uagent:
+		t_globals['archive'] = 'https://10.177.206.47:81/archive'
+
 class Home:
 	def GET(self):
+		parse_globals()
+
 		db=model.get_top_db(True)
 		ir=model.get_latest_ir()
 		release=model.get_latest_release()
-
-		uagent = web.ctx.env.get('HTTP_USER_AGENT')
-		if 'iPad; CPU OS 7_1' in uagent:
-			t_globals['archive'] = 'https://10.177.206.47:81/archive'
 
 		return render.index(release,ir,db)
 
 class DailyBuild:
 	def GET(self):
+		parse_globals()
+
 		latest = model.get_top_db(False)
 		builds = model.get_top_dbs()
 
@@ -30,6 +35,8 @@ class DailyBuild:
 
 class InternalRelease:
 	def GET(self):
+		parse_globals()
+
 		result = model.get_ir_all()
 		groups = model.group_versions(2)
 		return render.internalrelease(result,groups,'')
@@ -37,6 +44,8 @@ class InternalRelease:
 
 class InternalReleaseVersion:
 	def GET(self, version):
+		parse_globals()
+
 		result = model.get_ir_builds(version)
 		groups = model.group_versions(2)
 		return render.internalrelease(result,groups,version)
@@ -44,12 +53,16 @@ class InternalReleaseVersion:
 
 class Release:
 	def GET(self):
+		parse_globals()
+
 		result = model.get_release_all()
 		groups = model.group_versions(3)
 		return render.release(result,groups,'')
 
 class ReleaseVersion:
 	def GET(self,version):
+		parse_globals()
+		
 		result = model.get_release_builds(version)
 		groups = model.group_versions(3)
 		return render.release(result,groups,version)
