d1a38a92d71133cababbddfed4389956dc19f93f
张 锋 <zhangfeng0408@gmail.com>
2014-08-13 13:22:09 +0800

    fixed bug #5692

--- Trunk/Src/Common/Utility/DChartWrapper/DCTrendWrapper.m
+++ Trunk/Src/Common/Utility/DChartWrapper/DCTrendWrapper.m
@@ -177,7 +177,7 @@
     DCXYSeries* s = [[DCXYSeries alloc]initWithEnergyData:datas];
     s.symbolType = [self getSymbolTypeByIndex:index];
     s.symbolSize = style.symbolSize;
-    s.color = [REMColor colorByIndex:index];
+    s.color = targetEnergy.target.type == REMEnergyTargetBenchmarkValue ? self.style.benchmarkColor : [REMColor colorByIndex:index];
     s.target = targetEnergy.target;
     
     s.seriesKey = [self getSeriesKeyByTarget:s.target seriesIndex:index];
