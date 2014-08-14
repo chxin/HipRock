24248d61e246d6155c0c0468e3d27ff88ef75c52
zilong-oscar.xu <zilong-oscar.xu@schneider-electric.com>
2014-08-14 10:47:18 +0800

    bug fixed: stack order

--- Trunk/Src/Common/Utility/DChart/Series/DCColumnSeriesGroup.m
+++ Trunk/Src/Common/Utility/DChart/Series/DCColumnSeriesGroup.m
@@ -7,6 +7,9 @@
 //
 
 #import "DCColumnSeriesGroup.h"
+#import "_DCCoordinateSystem.h"
+#import "DCXYChartView.h"
+
 @interface DCColumnSeriesGroup()
 @property (nonatomic, strong) NSMutableArray* seriesList;
 @end
@@ -28,7 +31,14 @@
     return self.seriesList;
 }
 -(void)addSeries:(DCXYSeries*)series {
-    if (![self containsSeries:series]) [self.seriesList addObject:series];
+    NSArray* seriesList = self.coordinateSystem.chartView.seriesList;
+    if ([self containsSeries:series] || ![seriesList containsObject:series]) return;
+    [self.seriesList addObject:series];
+    self.seriesList = [[seriesList sortedArrayUsingComparator:^NSComparisonResult(DCXYSeries* obj1, DCXYSeries* obj2) {
+        NSUInteger index1 = [seriesList indexOfObject:obj1];
+        NSUInteger index2 = [seriesList indexOfObject:obj2];
+        return [@(index1) compare:@(index2)];
+    }] mutableCopy];
 }
 -(BOOL)containsSeries:(DCXYSeries*)series {
     return [self.seriesList containsObject:series];
