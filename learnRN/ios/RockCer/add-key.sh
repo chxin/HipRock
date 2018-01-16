db8ac2179349e9b6775f5c03f5410d64db8163d6
CHxin <chengxinhust@163.com>
2018-01-16 14:34:48 +0800

    security set-keychain-setting sudo require

--- learnRN/ios/RockCer/add-key.sh
+++ learnRN/ios/RockCer/add-key.sh
@@ -6,11 +6,11 @@ security create-keychain -p travis ios-build.keychain
 security default-keychain -s ios-build.keychain
 
 security unlock-keychain -p travis ios-build.keychain
-# security set-keychain-setting -t 3600 -l ~/Library/Keychains/ios-build.keychain
+security set-keychain-setting -t 3600 -l ~/Library/Keychains/ios-build.keychain
 
 security import ./ios_distribution.cer -A -k ~/Library/Keychains/ios-build.keychain
 security import ./dist.p12 -A -k ~/Library/Keychains/ios-build.keychain -P 123456
-security set-key-partition-list -S apple-tool:,apple: -s -k travis ios-build.keychain
+security set-key-partition-list -S apple-tool:,apple:,codesign: -s -k travis ios-build.keychain
 
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 cp ./profile/hocEDisco.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/
