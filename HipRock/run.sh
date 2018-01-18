d26aee232ae4bb7a43c28e74cce23be578dc74e8
CHxin <chengxinhust@163.com>
2018-01-18 16:38:51 +0800

    android shell HipRock

--- HipRock/run.sh
+++ HipRock/run.sh
@@ -1,4 +1,6 @@
 oldVer=`awk -F= '/ROCK_VERSION/{print $2}' android/gradle.properties |tail -n 1`
+currCommit=$(git rev-parse --short HEAD)
+filePath="./android/app/build/outputs/apk/app-internal-release-$version.apk"
 
 # echo "Please input the new version?The old version is:$oldVer"
 # read version
@@ -19,10 +21,6 @@ source configEnvScripts/findAndReplace.sh && getFileAndChangeJcenter
 cd android && ./gradlew assembleRelease
 cd ..
 
-rm ../build_file/*unaligned.apk
-
-filePath="./android/app/build/outputs/apk/app-internal-release-$version.apk"
-
-currCommit=$(git rev-parse --short HEAD)
+# rm ../build_file/*unaligned.apk
 
 curl -F "file=@$filePath" -F "uKey= 7d42c69844b88157360fe2dc141fdf1a" -F "_api_key= be1290e71bb2fab7a9547cda2ee37d7b" -F "installType=2" -F "password=123456" -F "updateDescription=$currCommit" http://qiniu-storage.pgyer.com/apiv1/app/upload
