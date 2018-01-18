a7ff78afe0eaf6150686258130a9ba449ab2a09c
CHxin <chengxinhust@163.com>
2018-01-18 15:21:46 +0800

    ios travis HipRock shell

--- HipRock/ios/RockCer/add-key.sh
+++ HipRock/ios/RockCer/add-key.sh
@@ -9,12 +9,12 @@ security lock-keychain -a
 security unlock-keychain -p travis ios-build.keychain
 security set-keychain-setting -t 3600 -l ~/Library/Keychains/ios-build.keychain
 
-security import ./ios_distribution.cer -A -k ~/Library/Keychains/ios-build.keychain
-security import ./dist.p12 -A -k ~/Library/Keychains/ios-build.keychain -P 123456
+security import ./ios/RockCer/ios_distribution.cer -A -k ~/Library/Keychains/ios-build.keychain
+security import ./ios/RockCer/dist.p12 -A -k ~/Library/Keychains/ios-build.keychain -P 123456
 security set-key-partition-list -S apple-tool:,apple:,codesign: -s -k travis ios-build.keychain
 
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
-cp ./profile/hocEDisco.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/
+cp ./ios/RockCer/profile/hocEDisco.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/
 
 security find-identity -p codesigning ~/Library/Keychains/ios-build.keychain
 security list-keychains
