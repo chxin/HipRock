3ed51260716fcadfd478dcf66993a24212b86249
CHxin <chengxinhust@163.com>
2018-01-19 19:29:21 +0800

    update shell

--- HipRock/buildTestIpa.sh
+++ HipRock/buildTestIpa.sh
@@ -1,7 +1,11 @@
-source ./buildNumber.sh
-currCommit=$(git rev-parse --short HEAD)
-buildNumber=`getParamAndPlusone`
+api_key=be1290e71bb2fab7a9547cda2ee37d7b
+appKey=8d52314a63156de90abff8a1413e7b8a
+uKey=7d42c69844b88157360fe2dc141fdf1a
 
+pgyerLog=`curl -d "_api_key=$api_key" -d "appKey=$appKey" https://www.pgyer.com/apiv2/app/view `
+currCommit=$(git rev-parse --short HEAD)
+buildNumber=$(echo $pgyerLog | tr ',' '\n' | awk -F : '/buildUpdateDescription/{print $2}'| sed 's/"//g' | head -1 | awk -F _ '{print $2}')
+buildNumber=$(($buildNumber+1))
 description="$currCommit"_"$buildNumber"
 
 OUTPUTDIR="./buildIOSTemp"
@@ -10,14 +14,14 @@ SCHEMEPROD="HipRock_Prod"
 APP_PROJECTPATH="./ios/HipRock.xcodeproj"
 PLIST_PATH='./ios/HipRock/Info.plist'
 
-# echo "Please enter the version?like the 1.0.0"
-# read version
-# while([[ $version == '' ]])
-# do
-# echo "Error! Please enter the version?like the 1.0.0"
-# read version
-# done
-version=`/usr/libexec/PlistBuddy -c "Print :CFBundleShortVersionString" ./ios/HipRock/Info.plist`
+oldVer=`/usr/libexec/PlistBuddy -c "Print :CFBundleShortVersionString" ./ios/HipRock/Info.plist`
+version=$(echo $pgyerLog | tr ',' '\n' | awk -F : '/buildVersion/{print $2}' | head -1 | sed 's/"//g')
+if [ $version == $oldVer ]
+then 
+	version=${version%.*}.$((${version##*.}+1))
+# else 
+# 	version=$oldVer	
+fi
 
 APPNAME='HipRock_V'$version
 
@@ -27,9 +31,7 @@ git checkout ${PLIST_PATH}
 rm "$OUTPUTDIR/$APPNAME/$SCHEMETEST.ipa"
 xcodebuild -project "$APP_PROJECTPATH" -scheme "$SCHEMETEST" archive -archivePath "$OUTPUTDIR/$APPNAME.xcarchive" -quiet
 xcodebuild -exportArchive -archivePath "$OUTPUTDIR/$APPNAME.xcarchive" -exportPath "$OUTPUTDIR/$APPNAME" -exportOptionsPlist "exportTestOptions.plist" -quiet
-curl -F "file=@$OUTPUTDIR/$APPNAME/$SCHEMETEST.ipa" -F 'uKey=7d42c69844b88157360fe2dc141fdf1a' -F '_api_key=be1290e71bb2fab7a9547cda2ee37d7b' -F "installType=2" -F "password=123456" -F "updateDescription=$description" 'https://www.pgyer.com/apiv1/app/upload'
-
-# curl -F "file=@$OUTPUTDIR/$APPNAME/$SCHEMETEST.ipa" -F "uKey= 24af41e3b5e5117e773a733378aefa29" -F "_api_key= 0691c7489e57a5158796f6e1e7e988bd" -F "installType=2" -F "password=123456" -F "updateDescription=$description" http://qiniu-storage.pgyer.com/apiv1/app/upload
+curl -F "file=@$OUTPUTDIR/$APPNAME/$SCHEMETEST.ipa" -F "uKey=$uKey" -F "_api_key=$api_key" -F "installType=2" -F "password=123456" -F "updateDescription=$description" 'https://www.pgyer.com/apiv1/app/upload'
 
 # git checkout ${PLIST_PATH}
 # . ./mergeProd.sh
--- HipRock/run.sh
+++ HipRock/run.sh
@@ -1,18 +1,24 @@
-oldVer=`awk -F= '/ROCK_VERSION/{print $2}' android/gradle.properties |tail -n 1`
-currCommit=$(git rev-parse --short HEAD)
-filePath="./android/app/build/outputs/apk/app-internal-release-$version.apk"
+_api_key=be1290e71bb2fab7a9547cda2ee37d7b
+appKey=b31ce581c0d4b898270189b5962c44fd
+uKey=7d42c69844b88157360fe2dc141fdf1a
 
-# echo "Please input the new version?The old version is:$oldVer"
-# read version
-# while([[ $version == '' ]])
-# do
-# echo "Error! Please input the new version?The old version is:$oldVer"
-# read version
-# done
-version=1.0.0
-# sed -i '' 's/$oldVer/$version/g' android/gradle.properties
-# sed -i "s/0.5.2/0.5.3/g" android/gradle\.properties
-# sed -i '' "s/$oldVer/$version/g" `grep $oldVer -rl  android/gradle.properties`
+pgyerLog=`curl -d "_api_key=$_api_key" -d "appKey=$appKey" https://www.pgyer.com/apiv2/app/view `
+buildNumber=$(echo $pgyerLog | tr ',' '\n' | awk -F : '/buildUpdateDescription/{print $2}'| sed 's/"//g' | head -1 | awk -F _ '{print $2}')
+buildNumber=$(($buildNumber+1))
+currCommit=$(git rev-parse --short HEAD)
+description="$currCommit"_"$buildNumber"
+
+oldVer=`awk -F= '/ROCK_VERSION/{print $2}' android/gradle.properties |tail -n 1`
+version=$(echo $pgyerLog | tr ',' '\n' | awk -F : '/buildVersion/{print $2}' | head -1 | sed 's/"//g')
+if [ $version == $oldVer ]
+then 
+	version=${version%.*}.$((${version##*.}+1))
+# else 
+# 	version=$oldVer
+fi
+sed -i '' "s/$oldVer/$version/g" `grep $oldVer -rl  android/gradle.properties`
+
+filePath="./android/app/build/outputs/apk/app-internal-release-$version.apk"
 
 cp ./android/customModules/ShareModule.java ./node_modules/react-native/ReactAndroid/src/main/java/com/facebook/react/modules/share/ShareModule.java
 rm -rf ./node_modules/react-native-svg/android/build
@@ -21,6 +27,15 @@ source configEnvScripts/findAndReplace.sh && getFileAndChangeJcenter
 cd android && ./gradlew assembleRelease
 cd ..
 
-# rm ../build_file/*unaligned.apk
+cp ./android/app/build/outputs/apk/*.apk ../build_file
+rm ../build_file/*unaligned.apk
 
-curl -F "file=@$filePath" -F "uKey= 7d42c69844b88157360fe2dc141fdf1a" -F "_api_key= be1290e71bb2fab7a9547cda2ee37d7b" -F "installType=2" -F "password=123456" -F "updateDescription=$currCommit" http://qiniu-storage.pgyer.com/apiv1/app/upload
+curl -F "file=@$filePath" -F "uKey=$uKey" -F "_api_key=$_api_key" -F "installType=2" -F "password=123456" -F "updateDescription=$description" http://qiniu-storage.pgyer.com/apiv1/app/upload
+
+# echo "Please input the new version?The old version is:$oldVer"
+# read version
+# while([[ $version == '' ]])
+# do
+# echo "Error! Please input the new version?The old version is:$oldVer"
+# read version
+# done
